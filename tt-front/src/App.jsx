0e6146ff3463e12f58a2d945780334880118fa1c
Waliston Euripedes <82295321+wal-wizard@users.noreply.github.com>
2023-06-13 16:37:02 -0300

    Add files via upload

---
+++ tt-front/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+
+import MenuLeft from "./components/MenuLeft/MenuLeft";
+
+const App = () => {
+  return (
+    <div className="App-Container">
+      <MenuLeft />
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ tt-front/src/components/MenuLeft/MenuLeft.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./MenuLeft.css";
+
+const MenuLeft = () => {
+    return (
+        <div className="menuleft-container">
+            <div className="menu--itens">
+                <img src={require("../Images/logo-twitter-png.png")} alt="icone twitter" className="icon-tt"/>
+                <div className="menu--options">
+
+                    <div>
+                        <i class="bi bi-house-fill"></i>
+                        <a href="1">home</a>
+                    </div>
+
+                    <div>
+                        <i class="bi bi-hash"></i>
+                        <a href="2">explore</a>
+                    </div>
+
+                    <div>
+                        <i class="bi bi-bell-fill"></i>
+                        <a href="2">notifications</a>
+                    </div>
+
+                    <div>
+                        <i class="bi bi-envelope"></i>
+                        <a href="3">messges</a>
+                    </div>
+
+                    <div>
+                        <i class="bi bi-bookmark"></i>
+                        <a href="4">bookmarks</a>
+                    </div>
+
+                    <div>
+                        <i class="bi bi-card-list"></i>
+                        <a href="5">lists</a>
+                    </div>
+
+                    <div>
+                        <i class="bi bi-person"></i>
+                        <a href="6">profile</a>
+                    </div>
+
+                    <div>
+                        <i class="bi bi-plus-circle"></i>
+                        <a href="7">more</a>
+                    </div>
+
+                </div>
+                <div>
+                    <button className="btn-primary">tweet</button>
+                </div>
+                <div className="profile--section">
+                    <div className="profile-pic">
+                        <img src={require("../Images/icon.png")} alt="profile pic" className="profile-pic-icon" />
+                    </div>
+                    <div className="name-aroba">
+                        <h4>Jamal Malik</h4>
+                        <p>@jamalmalik</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+
+export default MenuLeft;
