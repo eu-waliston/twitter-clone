5af9c137feb39507db210af1f290e0df85b8ee9a
Waliston <goneisgone@hotmail.com>
2023-06-15 19:47:45 -0300

    TT Clone - Left Menu - sTYLE

--- tt-front/src/components/RightBar/RightBar.jsx
+++ tt-front/src/components/RightBar/RightBar.jsx
@@ -4,9 +4,13 @@ import "./RightBar.css";
 const RightBar = () => {
   return (
     <div className="rightbar--section">
+      <div className="search--section">
+        <i class="bi bi-search"></i>
+        <input type="text" placeholder="Search Twitter" />
+      </div>
 
     </div>
-  )
-}
+  );
+};
 
 export default RightBar;
