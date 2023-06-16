8792606b79c8ff4fd6cd4df6de58fb3397e355d8
Waliston <goneisgone@hotmail.com>
2023-06-16 20:58:31 -0300

    TT clone - update

--- tt-front/src/App.jsx
+++ tt-front/src/App.jsx
@@ -4,11 +4,13 @@ import "./App.css";
 
 import MenuLeft from "./components/MenuLeft/MenuLeft";
 import RightBar from "./components/RightBar/RightBar";
+import Home from "./components/Home/Home";
 
 
 const App = () => {
   return (
     <div className="App-Container">
+      <Home />
       <RightBar /> 
       <MenuLeft />
            
