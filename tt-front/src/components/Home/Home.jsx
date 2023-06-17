72c9094221f3bdae934c7e331ba5c7b81262462e
Waliston <goneisgone@hotmail.com>
2023-06-16 21:10:08 -0300

    TT clone - Home Component

--- tt-front/src/components/Home/Home.jsx
+++ tt-front/src/components/Home/Home.jsx
@@ -4,7 +4,11 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home__component">
-      
+      <div className="home">
+          <h1>Home</h1>
+          <i class="bi bi-stars"></i>
+      </div>
+      <div className="separator"></div>
     </div>
   )
 }
